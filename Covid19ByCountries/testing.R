0111a016b2e0466310dfe3987ce42d1170d471f8
long_vo <corazon@LAPTOP-6KAVQSNN.localdomain>
2021-05-22 00:35:23 +0700

    add deathrate and chart

--- Covid19ByCountries/testing.R
+++ Covid19ByCountries/testing.R
@@ -2,6 +2,8 @@ library(dplyr)
 library(stringr)
 library(ggplot2)
 
+library(ggplot2)
+library(ggpubr)
 # Import data from csv
 Rawfile <- read.csv("owid-covid-data.csv")
 
@@ -19,7 +21,7 @@ glimpse(SimpleCountries)
 
 
 # Filter for specific_country and cleaning data
-specific_country <- 'Vietnam'
+specific_country <- 'World'
 SimpleVietnam <- SimpleCountries %>%
   filter (location == specific_country) %>%
   arrange(date)
@@ -37,14 +39,22 @@ weekly <- cbind(id=as.numeric(id), weekly)
 glimpse(weekly)
 
 WeeklyVietnam <- weekly %>%
-  mutate(week=id%/%7) %>%
+  mutate(week=id%/%1) %>%
   group_by(week) %>%
   summarise(week, weekly_cases=sum(new_cases), weekly_deaths=sum(new_deaths)) %>%
   arrange(week) %>%
+  mutate(deathrate = ifelse(weekly_cases == 0, 0, weekly_deaths / weekly_cases)) %>%
   distinct(week, .keep_all=TRUE)
-  
 glimpse(WeeklyVietnam)
 
 # TODO: collect this script into function: 
 #     [df] GetWeeklyData(string country)
 
+wdata <- WeeklyVietnam%>%
+  select(c(deathrate))
+# gghistogram(wdata, x = "deathrate", bins = 30, 
+#             fill = "#0073C2FF", color = "#0073C2FF",
+#             add = "mean", rug = TRUE)
+ggdensity(wdata, x = "deathrate", 
+          fill = "#0073C2FF", color = "#0073C2FF",
+          add = "mean", rug = TRUE)
